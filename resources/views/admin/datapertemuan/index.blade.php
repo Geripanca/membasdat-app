69c26dad7da8dfa123df93779abd0b891bfcfdb9
geri35 <geri@upi.edu>
2025-09-26 02:44:39 +0700

    add :modal tambah pertemuan

--- resources/views/admin/datapertemuan/index.blade.php
+++ resources/views/admin/datapertemuan/index.blade.php
@@ -27,11 +27,15 @@
   <div class="col-md-12 col-lg-12 order-2 mb-4">
     <div class="card h-100">
        <div class="card-header d-flex align-items-center justify-content-between mb-2">
-    <div class="justify-content-start">
-      <a href="{{ route('datapertemuan.create') }}" class="btn btn-xs btn-dark fw-bold p-2">
-        <i class='bx bx-joystick fs-6'></i>&nbsp;TAMBAH PERTEMUAN
-      </a>
-    </div>
+<div class="justify-content-start">
+  <button type="button" 
+          class="btn btn-xs btn-dark fw-bold p-2 buttonAddMeeting"
+          data-bs-toggle="modal" 
+          data-bs-target="#formModalAdminMeeting">
+    <i class='bx bx-joystick fs-6'></i>&nbsp;TAMBAH PERTEMUAN
+  </button>
+</div>
+
     <div class="justify-content-end">
       <form action="">
         <div class="input-group">
@@ -103,3 +107,59 @@
   </div>
 </div>
 @endsection
+<!-- Modal Tambah Pertemuan -->
+<div class="modal fade" id="formModalAdminMeeting" data-bs-backdrop="static" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <form action="{{ route('datapertemuan.store') }}" method="post" class="modalAdminMeeting">
+      @csrf
+      <div class="modal-content">
+        <div class="modal-header d-flex justify-content-between">
+          <h5 class="modal-title text-primary fw-bold">
+            Tambah Pertemuan&nbsp;<i class='bx bx-calendar-plus fs-5' style="margin-bottom: 1px;"></i>
+          </h5>
+          <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-dismiss="modal">
+            <i class="bx bx-x-circle text-danger fs-4" title="Tutup"></i>
+          </button>
+        </div>
+
+        <div class="modal-body">
+          <!-- Judul Pertemuan -->
+          <div class="row">
+            <div class="col mb-3">
+              <label for="judul" class="form-label required-label">Judul Pertemuan</label>
+              <input type="text" id="judul" name="judul" value="{{ old('judul') }}"
+                class="form-control @error('judul') is-invalid @enderror"
+                placeholder="Masukkan judul pertemuan" autocomplete="off" required>
+              @error('judul')
+              <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+          </div>
+
+          <!-- Deskripsi -->
+          <div class="row">
+            <div class="col mb-3">
+              <label for="deskripsi" class="form-label required-label">Deskripsi</label>
+              <textarea id="deskripsi" name="deskripsi" rows="4"
+                class="form-control @error('deskripsi') is-invalid @enderror"
+                placeholder="Masukkan deskripsi pertemuan" autocomplete="off" required>{{ old('deskripsi') }}</textarea>
+              @error('deskripsi')
+              <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">
+            <i class='bx bx-share fs-6' style="margin-bottom: 3px;"></i>&nbsp;Batal
+          </button>
+          <button type="submit" class="btn btn-primary">
+            <i class='bx bx-paper-plane fs-6' style="margin-bottom: 3px;"></i>&nbsp;Tambah
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
